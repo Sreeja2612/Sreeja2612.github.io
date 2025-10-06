5a29b81b39662d0161c7d0c7efd9c5df962a340b
Sreeja2612 <sreejagangavarapu2612@gmail.com>
2025-10-06 11:51:58 -0400

    Initial public release — refreshed October 2025

---
+++ src/app/application/application.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import {NgbTooltipModule} from "@ng-bootstrap/ng-bootstrap";
+import { FormsModule } from '@angular/forms';
+import {ApplicationComponent} from './application.component';
+import {HomeComponent} from "../home/home.component";
+import {NavbarComponent} from "../navbar/navbar.component";
+import {AboutComponent} from "../about/about.component";
+import {QuoteComponent} from "../quote/quote.component";
+import {PortfolioComponent} from "../portfolio/portfolio.component";
+import {ExperienceComponent} from "../experience/experience.component";
+// import {TrainingComponent} from "../training/training.component"; // 
+// import {AchievementComponent} from "../achievement/achievement.component";
+import {EducationComponent} from "../education/education.component";
+import {ContactComponent} from "../contact/contact.component";
+import {SocialComponent} from "../social/social.component";
+import {FooterComponent} from "../footer/footer.component";
+import {RouterModule, Routes} from "@angular/router";
+
+const routes: Routes = [{ path: '', component: ApplicationComponent }];
+
+@NgModule({
+	declarations: [
+		ApplicationComponent,
+		HomeComponent,
+		NavbarComponent,
+		AboutComponent,
+		QuoteComponent,
+		PortfolioComponent,
+		// TrainingComponent,
+		ExperienceComponent,
+		EducationComponent,
+		ContactComponent,
+		SocialComponent,
+		FooterComponent
+	],
+	imports: [
+		// Change this line:
+		// RouterModule.forChild(routes), // Now uses the const 'routes'
+		RouterModule.forChild([{ path: '', component: ApplicationComponent }]),
+		CommonModule,
+		NgbTooltipModule,
+		FormsModule
+	]
+})
+export class ApplicationModule { }
